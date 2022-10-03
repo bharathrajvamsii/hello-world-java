9e424f1dfa295fe3a17ac5797f7404063e415b8f
bharathraj <112840420+bharathrajvamsii@users.noreply.github.com>
2022-10-04 00:51:02 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello world!");
     System.out.println("Hello guru gggg kukka");
+    System.out.println("Hello guru gggg kukkk") ;
   }
 }
